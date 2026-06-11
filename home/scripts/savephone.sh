608aeaf8c11555700f94f90400f4bf1c94cb4e23
Matschieu <mathieudietsch@yahoo.fr>
2026-06-11 08:55:39 +0200

    Improve and update savephone

--- home/scripts/savephone.sh
+++ home/scripts/savephone.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-PRF_FILE='/home/mathieu/.unison/phone.prf'
-DEST='/media/mathieu/Data/Mathieu/Téléphone/Galaxy A3 2017/'
+HOME_SRC=`echo ~`
+PRF_FILE=$HOME_SRC'/.unison/phone.prf'
+DATA_SRC="/media/$USER/Data"
+DEST="/media/$USER/Data/Mathieu/Téléphone/Galaxy S21"
 GVFS='/run/user/1000/gvfs/'
 
 function append_phone_prf() {
@@ -9,9 +11,9 @@ function append_phone_prf() {
 }
 
 cd $GVFS
-MTP=`ls` 
 
-if [[ -z "$MTP" ]]; then
+ls | wc -l > /dev/null
+if [[ $? -eq 0 ]]; then
 	echo "No phone found"
 	exit 1
 fi
@@ -19,7 +21,12 @@ fi
 echo "BEFORE CONTINUING, AUTORISE THE COMPUTER TO READ THE PHONE MEMORY"
 read
 
-SRC="$GVFS$MTP/Card"
+cd "`ls`"
+cd "`ls`"
+
+SRC="`pwd`"
+
+echo "From $SRC to $DEST"
 
 touch $PRF_FILE
 cp -f $PRF_FILE ${PRF_FILE}.save
@@ -29,16 +36,22 @@ append_phone_prf "# $PRF_FILE"
 append_phone_prf "# Unison preferences file"
 append_phone_prf "root = $SRC"
 append_phone_prf "root = $DEST"
-append_phone_prf "ignore = Path DCIM Galaxy A3"
+append_phone_prf "ignore = Path *-GA32*"
 append_phone_prf "ignore = Path LOST.DIR"
+append_phone_prf "ignore = Path Alarms"
 append_phone_prf "ignore = Path Android"
+append_phone_prf "ignore = Path Movies"
+append_phone_prf "ignore = Path Music"
+append_phone_prf "ignore = Path Notifications"
+append_phone_prf "ignore = Path Ringtones"
+append_phone_prf "ignore = Path Sauvegarde"
 append_phone_prf "perms = 0"
 append_phone_prf "times = true"
 
-CMD="unison phone -force $SRC -auto"
-
-echo $CMD
+echo "Unison profile:"
 cat $PRF_FILE
 
+CMD="unison phone -force $SRC -auto"
+echo "Running $CMD"
 $CMD
 
